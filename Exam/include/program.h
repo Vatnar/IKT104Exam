c46ed3b95e296dc2b6e0dfd2a725ab75a822c118
Peter Vatnar Andersson <peterva@uia.no>
2025-04-14 15:56:26 +0200

    Added program headers and implementation

---
+++ Exam/include/program.h
@@ -0,0 +1,41 @@
+#include <mbed-os/mbed.h>
+#define DEMO 1
+
+enum class State {
+    STARTUP     = 1 << 0,
+    SHOWALARM   = 1 << 1,
+    EDITHOUR    = 1 << 2,
+    EDITMINUTE  = 1 << 3,
+    DATETIME    = 1 << 4,
+    TEMPHUMID   = 1 << 5,
+    WEATHER     = 1 << 6,
+    SETLOC      = 1 << 7,
+    NEWS        = 1 << 8
+};
+
+#ifdef DEMO
+class Display {
+    public:
+    void Init();
+    void EventLoop();
+};
+class API{
+    public:
+    void doStartupStuff(void *ptr);
+};
+#endif
+
+class Program {
+    public:
+    Program();
+    void Start();
+    private:
+    State m_state;
+
+    Display m_display;
+    Thread m_displayThread;
+
+    API m_api;
+    Thread m_apiThread;
+
+};
---
+++ Exam/source/program.cpp
@@ -0,0 +1,47 @@
+#include "program.h"
+#include <stdio.h>
+
+Program::Program(){
+    printf("Initializing program");
+    printf("."); printf(".\n");
+    
+    struct startupStruct {
+        time_t timestamp; 
+        nsapi_error_t code;
+    };
+    startupStruct m_apiargs;
+
+    // Henter UNIX timestamp og slikt
+    m_apiThread.start(callback([&]() {
+        m_api.doStartupStuff(&m_apiargs);
+    }));
+
+    // Initialiserer displayet
+    m_displayThread.start(callback(&m_display, &Display::Init));
+    
+    // Venter til threadsene er ferdige
+    m_displayThread.join();
+    m_apiThread.join();
+
+    // Setter staten til startup og starter eventloopen til displayet.
+    // Hvilke events som skal bli kjørt blir sendt fortløpende inn med thread flags og mailbox
+    m_displayThread.start(callback(&m_display, &Display::EventLoop));
+    
+    m_state = State::STARTUP;
+    m_displayThread.flags_set((uint32_t)m_state);
+
+    printf("Program constructed\n");
+}
+
+void Display::EventLoop(){
+    State state;
+    while (true){
+        #define ANY 0xFFFFFFFF
+        state = (State)ThisThread::flags_wait_any(ANY, false);
+        // Dette vil vente til en state er sendt, og behandle den
+        switch (state){
+            case State::STARTUP:    /*.....*/ break;
+            case State::SHOWALARM: /*.....*/ break;
+        }
+    }
+}
