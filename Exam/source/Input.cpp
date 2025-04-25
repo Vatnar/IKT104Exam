3f3e4cfde6735a4d2901f4ab76be81f8e4e1ebfd
Peter Vatnar Andersson <peter@70x7.net>
2025-04-25 14:08:56 +0200

    Thread ids fit now

--- Exam/source/Input.cpp
+++ Exam/source/Input.cpp
@@ -11,24 +11,26 @@ void Input::Init(osThreadId_t programThreadId){
 void Input::InputLoop(){
     while (true) {
         if (!m_left.read()) {
-            printf("\n INPUT: LEFT\n");
+            printf("\n INPUT: LEFTon thread \n");
+            printf("Input thread ID: %p\n", (void*)ThisThread::get_id());
+            printf("m_programThreadID: %p\n", m_programThreadId);
             osThreadFlagsSet(m_programThreadId, uint32_t(ButtonState::LEFT));
-            ThisThread::sleep_for(100ms); 
+            ThisThread::sleep_for(200ms); 
         }
         if (!m_up.read()) {
             printf("\n INPUT: UP\n");
             osThreadFlagsSet(m_programThreadId, uint32_t(ButtonState::UP));
-            ThisThread::sleep_for(100ms); 
+            ThisThread::sleep_for(200ms); 
         }
         if (!m_down.read()) {
             printf("\n INPUT: DOWN\n");
             osThreadFlagsSet(m_programThreadId, uint32_t(ButtonState::DOWN));
-            ThisThread::sleep_for(100ms); 
+            ThisThread::sleep_for(200ms); 
         }
         if (!m_right.read()) {
             printf("\n INPUT: RIGHT\n");
             osThreadFlagsSet(m_programThreadId, uint32_t(ButtonState::RIGHT));
-            ThisThread::sleep_for(100ms); 
+            ThisThread::sleep_for(200ms); 
         }
     }
 }
