9fa4585e72894cab0578a74ea56670f03014c80d
Oline Kylland Sæther <olineks@uia.no>
2025-04-25 11:32:00 +0200

    litt funksjoner

--- Exam/source/sensor.cpp
+++ Exam/source/sensor.cpp
@@ -7,5 +7,31 @@ DFRobot_RGBLCD1602 lcd(&lcdI2C);
 DevI2C i2c(PB_11, PB_10);
 HTS221Sensor hts221(&i2c);
 
+float humidity, temperature;
+void print_sensor(float temperature, float humidity){
+    lcd.printf("Temp: %.1f C\nHumidity: %.1f\%", temperature, humidity);
+};
+
+int main()
+{
+
 lcd.init();
+thread_sleep_for(20); 
 lcd.display();
+
+hts221.enable();
+hts221.init(NULL);
+thread_sleep_for(20); 
+
+hts221.reset();                         // Resets in case of sensor failure
+
+while (true) {                          // Loop to run the program indefinetly
+    hts221.get_humidity(&humidity);
+    hts221.get_temperature(&temperature);
+
+     thread_sleep_for(1000);                 // Sleeping for 1 second (1000ms)
+}
+
+
+return 0;
+}
