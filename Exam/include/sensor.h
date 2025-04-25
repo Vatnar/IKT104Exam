35ad0eca878124139ea3b2093d477fa102b47b45
Oline Kylland Sæther <olineks@uia.no>
2025-04-25 14:05:42 +0200

    i2c i klasse

--- Exam/include/sensor.h
+++ Exam/include/sensor.h
@@ -1,6 +1,10 @@
+#include "mbed.h"
+#include "DFRobot_RGBLCD1602.h"
+#include "HTS221Sensor.h"
 
 class TempHum {
     public:
+    TempHum();
     static float temperature;
     static float humidity;
 
@@ -10,4 +14,7 @@ class TempHum {
     private:
     float m_getTemperature();
     float m_getHumidity();
+
+    DevI2C i2c;
+    HTS221Sensor hts221;
 };
