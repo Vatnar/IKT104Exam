1daddebf872020774f5555428015503beeefbaf8
Emanuel Rønningsland <emanuelr@uia.no>
2025-04-25 13:59:55 +0200

    Small changes to Init()

--- Exam/include/display.h
+++ Exam/include/display.h
@@ -1,12 +1,17 @@
+#include "DFRobot_RGBLCD1602/DFRobot_RGBLCD1602.h"
+
 class Display
 {
 public:
     void Init();
     void EventLoop();
 
-private:                            // The functions are set as private because they
-    void m_displayStartup();        // are only being used by the public methods in the class
-    void m_displayDateTime();
+private:
+    I2C lcdI2C(D14, D15); 
+    DFRobot_RGBLCD1602 lcd(&lcdI2C); 
+    
+    void m_displayStartup();        // The functions are set as private because they
+    void m_displayDateTime();       // are only being used by the public methods in the class
     void m_displayTempHum();
     void m_displayWeather();
     void m_displayNews();
@@ -14,4 +19,6 @@ private:                            // The functions are set as private because 
     void m_editHour();
     void m_editMinute();
     void m_setLocation();
+
+    
 };
